ecf0e10a06f79a6922b715accd4ddb31836b54fd
Gineth <ginysp@hotmail.com>
2021-03-12 02:17:55 +0000

    include

---
+++ src/js/component/card.js
@@ -0,0 +1,139 @@
+import React from "react";
+
+export function Card() {
+	return (
+		<div className="row">
+			<div className="col-md-3">
+				<div className="card mb-3 shadow-sm">
+					<svg
+						className="bd-placeholder-img card-img-top"
+						width="100%"
+						height="225"
+						xmlns="http://www.w3.org/2000/svg"
+						role="img"
+						aria-label="Placeholder: 500 x 325"
+						preserveAspectRatio="xMidYMid slice"
+						focusable="false">
+						<title>Placeholder</title>
+						<rect width="100%" height="100%" fill="#55595c"></rect>
+						<text x="50%" y="50%" fill="#eceeef" dy=".3em">
+							500 x 325
+						</text>
+					</svg>
+
+					<div className="card-body">
+						<p className="card-text">
+							This is a wider card with supporting text below as a
+							natural lead-in to additional content. This content
+							is a little bit longer.
+						</p>
+						<div className="d-flex justify-content-between align-items-center">
+							<a href="#" className="btn btn-primary">
+								Find out more
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div className="col-md-3">
+				<div className="card mb-3 shadow-sm">
+					<svg
+						className="bd-placeholder-img card-img-top"
+						width="100%"
+						height="225"
+						xmlns="http://www.w3.org/2000/svg"
+						role="img"
+						aria-label="Placeholder: 500 x 325"
+						preserveAspectRatio="xMidYMid slice"
+						focusable="false">
+						<title>Placeholder</title>
+						<rect width="100%" height="100%" fill="#55595c"></rect>
+						<text x="50%" y="50%" fill="#eceeef" dy=".3em">
+							500 x 325
+						</text>
+					</svg>
+
+					<div className="card-body">
+						<p className="card-text">
+							This is a wider card with supporting text below as a
+							natural lead-in to additional content. This content
+							is a little bit longer.
+						</p>
+						<div className="d-flex justify-content-between align-items-center">
+							<a href="#" className="btn btn-primary">
+								Find out more
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div className="col-md-3">
+				<div className="card mb-3 shadow-sm">
+					<svg
+						className="bd-placeholder-img card-img-top"
+						width="100%"
+						height="225"
+						xmlns="http://www.w3.org/2000/svg"
+						role="img"
+						aria-label="Placeholder: 500 x 325"
+						preserveAspectRatio="xMidYMid slice"
+						focusable="false">
+						<title>Placeholder</title>
+						<rect width="100%" height="100%" fill="#55595c"></rect>
+						<text x="50%" y="50%" fill="#eceeef" dy=".3em">
+							500 x 325
+						</text>
+					</svg>
+
+					<div className="card-body">
+						<p className="card-text">
+							This is a wider card with supporting text below as a
+							natural lead-in to additional content. This content
+							is a little bit longer.
+						</p>
+						<div className="d-flex justify-content-between align-items-center">
+							<a href="#" className="btn btn-primary">
+								Find out more
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div className="col-md-3">
+				<div className="card mb-3 shadow-sm">
+					<svg
+						className="bd-placeholder-img card-img-top"
+						width="100%"
+						height="225"
+						xmlns="http://www.w3.org/2000/svg"
+						role="img"
+						aria-label="Placeholder: 500 x 325"
+						preserveAspectRatio="xMidYMid slice"
+						focusable="false">
+						<title>Placeholder</title>
+						<rect width="100%" height="100%" fill="#55595c"></rect>
+						<text x="50%" y="50%" fill="#eceeef" dy=".3em">
+							500 x 325
+						</text>
+					</svg>
+
+					<div className="card-body">
+						<p className="card-text">
+							This is a wider card with supporting text below as a
+							natural lead-in to additional content. This content
+							is a little bit longer.
+						</p>
+						<div className="d-flex justify-content-between align-items-center">
+							<a href="#" className="btn btn-primary">
+								Find out more
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
---
+++ src/js/component/footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Footer() {
+	return (
+		<footer className="bg-dark text-center text-white">
+			<div className="text-center p-3">© 2020 Copyright:</div>
+		</footer>
+	);
+}
